0012ead9b403d44521ada563c0c27c5c8187bc7a
nebula <miyamoto@brain.imi.i.u-tokyo.ac.jp>
2017-06-10 07:23:19 +0900

    refactor for next step

---
+++ src/benchmark_utils.h
@@ -0,0 +1,13 @@
+//
+// Created by nebula on 6/10/17.
+//
+
+#ifndef ASM_NEURONS_BENCHMARK_UTILS_H
+#define ASM_NEURONS_BENCHMARK_UTILS_H
+
+double diff_double(int size, double *array1, double *array2);
+void init_double(int size, double *array1, double value);
+void print_double(int x_size, int y_size, double *array);
+double getTime();
+
+#endif //ASM_NEURONS_BENCHMARK_UTILS_H
--- src/iz_main.c
+++ src/iz_main.c
@@ -2,47 +2,8 @@
 #include <stdlib.h>
 #include <math.h>
 
+#include "benchmark_utils.h"
 #include "iz.h"
-
-double diff_double(int size, double *array1, double *array2) {
-    int i;
-    double diff = 0.0;
-    for (i = 0; i < size; i++) {
-        diff += fabs(*array1 - *array2);
-        array1++;
-        array2++;
-    }
-    return diff;
-}
-
-void init_double(int size, double *array1, double value) {
-    int i;
-    for (i = 0; i < size; i++) {
-        *array1 = value;
-        array1++;
-    }
-}
-
-void print_double(int x_size, int y_size, double *array) {
-    int i, j;
-    for (i = 0; i < y_size; i++) {
-        for (j = 0; j < x_size; j++) {
-            printf(" %9.5f", *array);
-            array++;
-        }
-        printf("\n");
-    }
-}
-
-
-#include <sys/time.h>
-double getTime()
-{
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    return(tv.tv_sec + (double)tv.tv_usec*1e-6);
-
-}
 
 double *check_calc(void (*func)(int max_step, int n_cell, double *iz_const, double *iz_v, double *iz_u),
                    int max_step, int n_cell, double *iz_const,
